c1ed1e71916b2b3a27a73b2577bab02c5aacd45a
vishaljain091098 <vishaljain091098@gmail.com>
2024-03-27 08:37:20 +0000

    main js

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,8 @@ const config = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-themes",
+    "@storybook/addon-designs",
   ],
   framework: {
     name: "@storybook/react-vite",
